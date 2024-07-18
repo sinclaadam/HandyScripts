34ce8e9b6cf244f344883f93cccf765f6267f0cc
Adam Sinclair <adam.sinclair@gmail.com>
2024-07-18 19:01:21 +1200

    Restart Steam

---
+++ Restart Steam.ps1
@@ -0,0 +1,17 @@
+﻿$SteamExe = "C:\Program Files (x86)\Steam\steam.exe"
+
+Write-Host "Stopping Steam Process"
+$steam = Get-Process steam -ErrorAction SilentlyContinue
+if ($steam) {
+  # try gracefully first
+  &$SteamExe -shutdown
+  # kill after ten seconds
+  Sleep 10
+  if (!$steam.HasExited) {
+    Write-Host "Steam doesn't want to cooperate, killing it forcefully"
+    $steam | Stop-Process -Force
+  }
+}
+
+Write-Host "Starting Steam"
+Start-Process -FilePath $SteamExe
